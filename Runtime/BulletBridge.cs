129c6f72d68e02b887325d02f85e28c473e1a9a7
soapinduty <g4mrator@gmail.com>
2020-04-23 18:01:56 +0200

    Initial package commit.

---
+++ Runtime/BulletBridge.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Roboy.BulletBridge
+{
+    public class BulletBridge : MonoBehaviour
+    {
+        public static string Version = "0.1.0";
+        public GameObject Roboy_Model;
+
+        void Start()
+        {
+            InitializeBridge();
+        }
+
+
+        void Update()
+        {
+            SynchStates();
+        }
+
+        private void InitializeBridge()
+        {
+            //TODO
+            //Create Instances
+            //Spawn Objects
+        }
+
+        private void SynchStates()
+        {
+            //TODO
+            //Synch Object Positions and Rotations
+
+        }
+    }
+}
